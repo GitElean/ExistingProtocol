6c25c8b09015c13a00e66e63f5e5b9546155b09f
root <root@MSI>
2023-08-14 20:47:10 -0600

    doge añadido | librerias utilitarias | se añadio la clase para eliminar un usuario, no probada aún

--- XmppProtocol.py
+++ XmppProtocol.py
@@ -7,10 +7,12 @@ Call of libraries and plugins
 
 
 #Import of libraries
+import sys
+import logging
 from slixmpp.exceptions import IqError, IqTimeout
 from slixmpp.xmlstream.stanzabase import ET, ElementBase 
 import slixmpp
-
+import base64, time
 
 #----------------------------------------------
 #Register class
@@ -46,7 +48,7 @@ class RyE(slixmpp.ClientXMPP):
         except Exception as e:
             print(e)
             self.disconnect()
-
+#Clase hecha con la ayuda de copilot
 
 
 
@@ -57,6 +59,38 @@ class RyE(slixmpp.ClientXMPP):
 #----------------------------------------------
 #Delete account class
 #----------------------------------------------
+class Del(slixmpp.ClientXMPP):
+    def __init__(self, jid, password):
+        slixmpp.ClientXMPP.__init__(self, jid, password)
+
+        self.user = jid
+        self.add_event_handler("session_start", self.start)
+
+    def start(self, event):
+  
+        self.send_presence()
+        self.get_roster()
+
+        delete = self.Iq()
+        delete['type'] = 'set'
+        delete['from'] = self.user
+        fragment = ET.fromstring("<query xmlns='jabber:iq:register'><remove/></query>")
+        delete.append(fragment)
+
+        try:
+            delete.send()
+            print("Cuenta Borrada")
+        except IqError as e:
+           
+            print("Error", e)
+        except IqTimeout:
+
+            print("timeout del server")
+        except Exception as e:
+     
+            print(e)  
+
+        self.disconnect()
 
 
 #----------------------------------------------
